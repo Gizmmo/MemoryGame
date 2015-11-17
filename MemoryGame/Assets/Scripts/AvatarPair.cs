e8e0618636a71523122f26cf36222ae689fc4c80
Travis Scott <travisscott301@gmail.com>
2015-11-16 23:24:09 -0600

    Added further docs

--- MemoryGame/Assets/Scripts/AvatarPair.cs
+++ MemoryGame/Assets/Scripts/AvatarPair.cs
@@ -2,9 +2,14 @@
 
 public class AvatarPair {
 
-    public string Name;
-    public Sprite HeadShot;
+    public string Name; //The name of the Avatar
+    public Sprite HeadShot;  //The headshot for the avatar
 
+    /// <summary>
+    /// Constructor for the Avatar pair class
+    /// </summary>
+    /// <param name="headShot">The new headashot sprite for this avatar pair</param>
+    /// <param name="name">The new name for this avatar pair</param>
     public AvatarPair(Sprite headShot, string name) {
         Name = name;
         HeadShot = headShot;
