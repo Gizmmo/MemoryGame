dbb70b5a584d863fba65e17e18d2d4d80691ad81
Travis Scott <travisscott301@gmail.com>
2015-11-16 23:01:04 -0600

    Added Avatar Pair for storing headshot-name combos into memory

--- MemoryGame/Assets/Scripts/ActorAvatar.cs
+++ MemoryGame/Assets/Scripts/ActorAvatar.cs
@@ -16,25 +16,14 @@ public class ActorAvatar : MonoBehaviour {
     public Action<ActorAvatar> AvatarOnChange; // Event that triggers when the name or headshot of the avatar changes
     #endregion
 
-    void Start() {
-        RandomizeAvatar();
-    }
-
     #region Private Methods
-    /// <summary>
-    /// Randomize the name and sprite for this avatar.
-    /// </summary>
-    void RandomizeAvatar() {
-       // Sets the avatar to a random sprite and name
-       SetAvatar(AvatarManager.Instance.GetRandomSprite(), AvatarManager.Instance.GetRandomName());
-    }
 
     /// <summary>
     /// Sets the avatar to the name and sprite passed
     /// </summary>
     /// <param name="sprite">The new headshot for the avatar</param>
     /// <param name="avatarName">The new name for the avatar</param>
-    void SetAvatar(Sprite sprite, string avatarName) {
+    public void SetAvatar(Sprite sprite, string avatarName) {
 
         // Set the sprite and text of the Avatar
         AvatarImage.sprite = sprite;
--- MemoryGame/Assets/Scripts/Managers/AvatarManager.cs
+++ MemoryGame/Assets/Scripts/Managers/AvatarManager.cs
@@ -9,7 +9,41 @@ public class AvatarManager : Manager<AvatarManager> {
 
     [Tooltip("Strings that are used as names for the different people")]
     public List<string> Names;
+
+    public List<ActorAvatar> Avatars; 
+
+    private List<AvatarPair> _storedAvatarPairs = new List<AvatarPair>();
     #endregion
+
+    //Runs at the start
+    void Start() {
+        SetNewAvatars();
+    }
+
+    /// <summary>
+    /// Sets the list avatars to each a new random AvatarPair
+    /// </summary>
+    void SetNewAvatars() {
+        
+        // Clear out other stored Avatar Pairs
+        _storedAvatarPairs.Clear();
+
+        // For each Avatar Actor Component in the List...
+        for (var i = 0; i < Avatars.Count; i++) {
+
+            // ... Get a new sprtie and name...
+            var sprite = GetRandomSprite();
+            var avatarName = GetRandomName();
+
+            // ... and store the two random variables in a new AvatarPair...
+            _storedAvatarPairs.Add(new AvatarPair(sprite, avatarName));
+
+            // ...and set the avatar on screen to that respective pair.
+            Avatars[i].SetAvatar(sprite, avatarName);
+
+        }
+
+    }
 
     #region Public Methods
     /// <summary>
