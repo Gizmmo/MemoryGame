4c42f31421c60399f22f2fe1159da531cde50145
Travis Scott <travisscott301@gmail.com>
2015-11-17 23:51:42 -0600

    Changed to single button, added banner label, and added game states to the Avatar Manager class.  Currently only runs through 3 states

--- MemoryGame/Assets/Scripts/Managers/AvatarManager.cs
+++ MemoryGame/Assets/Scripts/Managers/AvatarManager.cs
@@ -1,5 +1,8 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class AvatarManager : Manager<AvatarManager> {
 
@@ -10,14 +13,28 @@ public class AvatarManager : Manager<AvatarManager> {
     [Tooltip("Strings that are used as names for the different people")]
     public List<string> Names;
 
-    public List<ActorAvatar> Avatars; 
+    public List<ActorAvatar> Avatars;
+
+    public Text Banner;
+    public Button ActionButton;
+    private Text _buttonText;
 
     private List<AvatarPair> _storedAvatarPairs = new List<AvatarPair>();
+
+    private enum GameState {
+        Start,
+        Memorize,
+        Countdown,
+        Choose,
+        Result,
+        Restart
+    }
+
+    private GameState _currentGameState = GameState.Start;
     #endregion
 
-    //Runs at the start
     void Start() {
-        SetNewAvatars();
+        _buttonText = ActionButton.GetComponentInChildren<Text>();
     }
 
     #region Public Methods
@@ -43,21 +60,83 @@ public class AvatarManager : Manager<AvatarManager> {
         avatar.SetAvatar(GetRandomSprite(), GetRandomName());
     }
 
+    public void ActionClick() {
+        switch (_currentGameState) {
+
+            case GameState.Start:
+                EnableAllAvatars();
+                _currentGameState = GameState.Memorize;
+                SetNewAvatars();
+                Banner.text = "Memorize!";
+                _buttonText.text = "Go!";
+                break;
+
+            case GameState.Memorize:
+                _currentGameState = GameState.Restart;
+                RandomizeAllAvatars();
+                Banner.text = "Choose the Correct Pairs!";
+                _buttonText.text = "Restart";
+                break;
+
+            case GameState.Restart:
+                _currentGameState = GameState.Start;
+                DisableAllAvatars();
+                Banner.text = "Click Start To Begin";
+                _buttonText.text = "Start";
+                break;
+            case GameState.Countdown:
+                break;
+            case GameState.Choose:
+                break;
+            case GameState.Result:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    /// <summary>
+    /// Enables all Avatars on Screen
+    /// </summary>
+    public void EnableAllAvatars() {
+        SetAvatarsEnabled(true);
+    }
+
+    /// <summary>
+    /// Disables All Avatars on Screen
+    /// </summary>
+    public void DisableAllAvatars() {
+        SetAvatarsEnabled(false);
+    }
+
     #endregion
 
     #region Private Methods
 
     /// <summary>
+    /// Sets all Avatars enabled attribute to the boolean passed
+    /// </summary>
+    /// <param name="isEnabled">Enables all Avatars if true, false if not</param>
+    private void SetAvatarsEnabled(bool isEnabled) {
+        
+        // For each Avatar in the Avatar array...
+        for (var i = 0; i < Avatars.Count; i++) {
+
+            // ..change the enable attribute to the bool passed
+            Avatars[i].gameObject.SetActive(isEnabled);
+        }
+    }
+
+    /// <summary>
     /// Sets the list avatars to each a new random AvatarPair
     /// </summary>
-    void SetNewAvatars() {
-        
+    private void SetNewAvatars() {
         // Clear out other stored Avatar Pairs
         _storedAvatarPairs.Clear();
 
         // For each Avatar Actor Component in the List...
         for (var i = 0; i < Avatars.Count; i++) {
-
+            
             // ... Get a new sprtie and name...
             var sprite = GetRandomSprite();
             var avatarName = GetRandomName();
@@ -67,16 +146,14 @@ public class AvatarManager : Manager<AvatarManager> {
 
             // ...and set the avatar on screen to that respective pair.
             Avatars[i].SetAvatar(sprite, avatarName);
-
         }
-
     }
 
     /// <summary>
     /// Returns a random Sprite from the Headshots list
     /// </summary>
     /// <returns>A Sprite headshot</returns>
-    Sprite GetRandomSprite() {
+    private Sprite GetRandomSprite() {
         return GetRandomListElement(Headshots);
     }
 
@@ -84,7 +161,7 @@ public class AvatarManager : Manager<AvatarManager> {
     /// Returns a random string from the Names list
     /// </summary>
     /// <returns>A String name</returns>
-    string GetRandomName() {
+    private string GetRandomName() {
         return GetRandomListElement(Names);
     }
 
@@ -95,8 +172,7 @@ public class AvatarManager : Manager<AvatarManager> {
     /// <param name="list">A list of T elements with one being returned</param>
     /// <param name="removeElement">if true, the element will be removed from the list once randomly chosen</param>
     /// <returns>An element of T type</returns>
-    static T GetRandomListElement<T>(IList<T> list, bool removeElement = false) {
-        
+    private static T GetRandomListElement<T>(IList<T> list, bool removeElement = false) {
         //Get a random number between 0 and the total list count
         var avatarPosition = Random.Range(0, list.Count);
 
@@ -112,5 +188,6 @@ public class AvatarManager : Manager<AvatarManager> {
         //Return the removed element
         return returnElement;
     }
+
     #endregion
 }
